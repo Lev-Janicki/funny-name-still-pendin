d4fcffe292e8d05e1147e56c703c470b25c26bcf
Lev-Janicki <72770454+Lev-Janicki@users.noreply.github.com>
2021-01-05 18:23:51 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 light.set_brightness(1060)
 while True:
     if input.light_level() <= 3:
+        pause(3000)
         light.set_all(color.rgb(255,0,255))
         pause(1000)
         light.clear()
-        pause(1000)
     else:
         light.clear()
--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 light.setBrightness(1060)
 while (true) {
     if (input.lightLevel() <= 3) {
+        pause(3000)
         light.setAll(color.rgb(255, 0, 255))
         pause(1000)
         light.clear()
-        pause(1000)
     } else {
         light.clear()
     }
